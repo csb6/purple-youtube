245f67c932ff0c250a9b7eb9eac5b3915f4e21f4
Cole Blakley <cblakley15@gmail.com>
2026-02-12 20:27:22 -0700

    Create Youtube.ChatClient class to handle interactions with YouTube Data API
    Internally does HTTP requests, exposes higher-level async API

--- main.c
+++ main.c
@@ -1,192 +1,38 @@
-#include <libsoup/soup.h>
 #include <glib.h>
 #include <glib/gprintf.h>
 #include <glib-object.h>
-#include <json-glib/json-glib.h>
-
-// TODO: https://developers.google.com/youtube/v3/live/docs/liveChatMessages
-//  - Seems like will have to poll using pollingIntervalMillis and nextPageToken
-
-#define YOUTUBE_API_BASE_URL "https://www.googleapis.com/youtube/v3/"
-
-#define cnt_of_array(arr) (sizeof(arr) / sizeof(arr[0]))
-
-typedef struct {
-    const char* name;
-    const char* value;
-} Param;
+#include "youtube_chat_client.h"
 
 static
-SoupMessage* create_youtube_message(const char* api_key, const char* resource, const Param* params, int param_count)
-{
-    GString* url = g_string_new(YOUTUBE_API_BASE_URL);
-    g_string_append(url, resource);
-    if(param_count > 0) {
-        g_string_append(url, "?");
-        for(int i = 0; i < param_count; ++i) {
-            g_string_append_uri_escaped(url, params[i].name, NULL, TRUE);
-            g_string_append(url, "=");
-            g_string_append_uri_escaped(url, params[i].value, NULL, TRUE);
-            if(i != param_count - 1) {
-                g_string_append(url, "&");
-            }
-        }
-    }
-    SoupMessage* request = soup_message_new(SOUP_METHOD_GET, url->str);
-    g_string_free(url, TRUE);
-    SoupMessageHeaders* headers = soup_message_get_request_headers(request);
-    soup_message_headers_append(headers, "x-goog-api-key", api_key);
-    return request;
-}
+void on_live_streams(GObject* source_object, GAsyncResult* result, gpointer data);
 
 static
-JsonNode* parse_json(GBytes* data)
+void on_channel_id(GObject* source_object, GAsyncResult* result, gpointer data)
 {
-    GError* err = NULL;
-    gsize data_size = 0;
-    gconstpointer data_ptr = g_bytes_get_data(data, &data_size);
-    JsonParser* parser = json_parser_new_immutable();
-    json_parser_load_from_data(parser, data_ptr, data_size, &err);
-    if(err) {
-        g_printerr("Failed to parse JSON response");
-        g_object_unref(parser);
-        return NULL;
-    }
-
-    JsonNode* response_root = json_parser_get_root(parser);
-    if(!response_root) {
-        g_printerr("Failed to get JSON root");
-        g_object_unref(parser);
-        return NULL;
-    }
-    json_node_ref(response_root);
-    g_object_unref(parser);
-    return response_root;
-}
-
-static
-JsonArray* match_json_path(JsonNode* root, const char* path)
-{
-    JsonPath* path_obj = json_path_new();
-    json_path_compile(path_obj, path, NULL);
-    JsonNode* results = json_path_match(path_obj, root);
-    JsonArray* results_array = json_node_get_array(results);
-    json_array_ref(results_array);
-    json_node_unref(results);
-    g_clear_object(&path_obj);
-    return results_array;
-}
-
-static
-char* fetch_channel_id(SoupSession* session, const char* api_key, const char* handle)
-{
-    Param params[] = {
-        {"part", "id"},
-        {"fields", "items/id"},
-        {"forHandle", handle}
-    };
-    SoupMessage* msg = create_youtube_message(api_key, "channels", params, cnt_of_array(params));
-
-    GError* err = NULL;
-    GBytes* response = soup_session_send_and_read(session, msg, NULL, &err);
-    g_clear_object(&msg);
-    if(err) {
-        g_printerr("Failed to fetch data");
-        return NULL;
-    }
-
-    JsonNode* response_root = parse_json(response);
-    g_bytes_unref(response);
-    if(!response_root) {
-        g_printerr("Failed to get JSON root");
-        return NULL;
-    }
-
-    JsonArray* results = match_json_path(response_root, "$['items'][0]['id']");
-    if(json_array_get_length(results) != 1) {
-        g_printerr("Unexpected channel ID results");
-        json_array_unref(results);
-        json_node_unref(response_root);
-        return NULL;
-    }
-
-    const char* id = json_array_get_string_element(results, 0);
-    if(!id) {
-        g_printerr("Unexpected value for channel ID");
-        json_array_unref(results);
-        json_node_unref(response_root);
-        return NULL;
-    }
-    char* id_str = g_strdup(id);
-    json_array_unref(results);
-    json_node_unref(response_root);
-    return id_str;
-}
-
-static
-GPtrArray* fetch_live_streams(SoupSession* session, const char* api_key, const char* channel_id)
-{
-    Param params[] = {
-        {"part", "snippet,id"},
-        {"fields","items/id/videoId"},
-        {"eventType", "live"},
-        {"channelId", channel_id},
-        {"order", "date"},
-        {"type", "video"}
-    };
-    SoupMessage* msg = create_youtube_message(api_key, "search", params, cnt_of_array(params));
-    GError* err = NULL;
-    GBytes* response = soup_session_send_and_read(session, msg, NULL, &err);
-    g_clear_object(&msg);
-    if(err) {
-        g_printerr("Failed to fetch data");
-        return NULL;
-    }
-
-    JsonNode* response_root = parse_json(response);
-    g_bytes_unref(response);
-    if(!response_root) {
-        g_printerr("Failed to get JSON root");
-        return NULL;
-    }
-    JsonArray* results = match_json_path(response_root, "$['items'][*]['id']['videoId']");
-    GPtrArray* live_streams = g_ptr_array_new();
-    guint result_count = json_array_get_length(results);
-    for(guint i = 0; i < result_count; ++i) {
-        const char* video_id = json_array_get_string_element(results, i);
-        if(!video_id) {
-            g_warning("Unexpected format for video ID");
-            continue;
-        }
-        g_ptr_array_add(live_streams, g_strdup(video_id));
-    }
-    json_array_unref(results);
-    json_node_unref(response_root);
-    return live_streams;
-}
-
-int main(int argc, char** argv)
-{
-    if(argc != 2) {
-        g_printerr("Usage: %s channel_handle\n", argv[0]);
-        return 1;
-    }
-    SoupSession* session = soup_session_new();
-    gchar* api_key = (gchar*)g_getenv("YT_API_KEY");
-    if(!api_key) {
-        g_printerr("Environment variable YT_API_KEY must be set to a YouTube Data API key\n");
-        return 1;
-    }
-    api_key = g_strdup(api_key);
-    const char* channel_handle = argv[1];
-    char* channel_id = fetch_channel_id(session, api_key, channel_handle);
-    if(!channel_id) {
-        g_free(api_key);
-        return 1;
+    YoutubeChatClient* client = (YoutubeChatClient*)source_object;
+    GError* error = NULL;
+    char* channel_id = youtube_chat_client_get_channel_id_finish(client, result, &error);
+    if(error) {
+        g_printerr("Failed to get channel ID\n");
+        return;
     }
     g_printf("Channel ID: %s\n", channel_id);
+
+    youtube_chat_client_get_live_streams_async(client, g_strdup(channel_id), NULL, on_live_streams, data);
+    g_free(channel_id);
+}
+
+static
+void on_live_streams(GObject* source_object, GAsyncResult* result, gpointer data)
+{
+    YoutubeChatClient* client = (YoutubeChatClient*)source_object;
+    GError* error = NULL;
     g_printf("Active live streams:\n");
-    GPtrArray* live_streams = fetch_live_streams(session, api_key, channel_id);
+    GPtrArray* live_streams = youtube_chat_client_get_live_streams_finish(client, result, &error);
+    if(error) {
+        g_printerr("Failed to get live streams\n");
+        return;
+    }
     guint live_stream_count = live_streams->len;
     for(guint i = 0; i < live_stream_count; ++i) {
         g_printf("%s\n", (const char*)live_streams->pdata[i]);
@@ -196,7 +42,32 @@ int main(int argc, char** argv)
         g_free(live_streams->pdata[i]);
     }
     g_ptr_array_free(live_streams, TRUE);
-    g_free(channel_id);
+    g_main_loop_quit((GMainLoop*)data);
+}
+
+int main(int argc, char** argv)
+{
+    if(argc != 2) {
+        g_printerr("Usage: %s channel_handle\n", argv[0]);
+        return 1;
+    }
+    gchar* api_key = (gchar*)g_getenv("YT_API_KEY");
+    if(!api_key) {
+        g_printerr("Environment variable YT_API_KEY must be set to a YouTube Data API key\n");
+        return 1;
+    }
+    api_key = g_strdup(api_key);
+    const char* channel_handle = argv[1];
+
+    GMainLoop* main_loop = g_main_loop_new(g_main_context_default(), /*is_running=*/FALSE);
+
+    YoutubeChatClient* client = youtube_chat_client_new(api_key);
+    youtube_chat_client_get_channel_id_async(client, channel_handle, NULL, on_channel_id, main_loop);
+
+    g_main_loop_run(main_loop);
+    g_main_loop_unref(main_loop);
+    g_object_unref(client);
     g_free(api_key);
+
     return 0;
 }
---
+++ youtube_chat_client.c
@@ -0,0 +1,262 @@
+#include "youtube_chat_client.h"
+#include <glib.h>
+#include <json-glib/json-glib.h>
+#include <libsoup/soup.h>
+
+// TODO: https://developers.google.com/youtube/v3/live/docs/liveChatMessages
+//  - Seems like will have to poll using pollingIntervalMillis and nextPageToken
+
+#define YOUTUBE_API_BASE_URL "https://www.googleapis.com/youtube/v3/"
+
+#define cnt_of_array(arr) (sizeof(arr) / sizeof(arr[0]))
+
+typedef struct {
+    const char* name;
+    const char* value;
+} Param;
+
+struct _YoutubeChatClient {
+    GObject parent_instance;
+    SoupSession* session;
+    char* api_key;
+};
+G_DEFINE_TYPE(YoutubeChatClient, youtube_chat_client, G_TYPE_OBJECT)
+//G_DEFINE_DYNAMIC_TYPE_EXTENDED(YoutubeChatClient, youtube_chat_client, G_TYPE_OBJECT,
+//                               G_TYPE_FLAG_FINAL, {})
+
+static
+SoupMessage* create_youtube_message(const char* api_key, const char* resource, const Param* params, int param_count);
+
+static
+JsonNode* parse_json(GBytes* data);
+
+static
+JsonArray* match_json_path(JsonNode* root, const char* path);
+
+
+static
+void youtube_chat_client_init(YoutubeChatClient* client)
+{
+    client->session = soup_session_new();
+}
+
+static
+void youtube_chat_client_finalize(GObject* obj)
+{
+    YoutubeChatClient* client = YOUTUBE_CHAT_CLIENT(obj);
+    g_clear_object(&client->session);
+    g_free(client->api_key);
+    G_OBJECT_CLASS(youtube_chat_client_parent_class)->finalize(obj);
+}
+
+//static
+//void youtube_chat_client_class_finalize(YoutubeChatClientClass* klass)
+//{}
+
+static
+void youtube_chat_client_class_init(YoutubeChatClientClass* klass)
+{
+    GObjectClass* obj_class = G_OBJECT_CLASS(klass);
+
+    obj_class->finalize = youtube_chat_client_finalize;
+
+    // TODO: signals/properties
+}
+
+YoutubeChatClient* youtube_chat_client_new(const char* api_key)
+{
+    YoutubeChatClient* client = g_object_new(YOUTUBE_TYPE_CHAT_CLIENT, NULL);
+    client->api_key = g_strdup(api_key);
+    return client;
+}
+
+static
+void on_channel_id_response(GObject* source_object, GAsyncResult* result, gpointer data)
+{
+    GError* error = NULL;
+    SoupSession* session = SOUP_SESSION(source_object);
+    GTask* task = (GTask*)data;
+    GBytes* response = soup_session_send_and_read_finish(session, result, &error);
+    JsonNode* response_root = NULL;
+    JsonArray* results = NULL;
+
+    if(error) {
+        g_printerr("Failed to fetch data");
+        g_task_return_error(task, error);
+        goto cleanup;
+    }
+
+    response_root = parse_json(response);
+    if(!response_root) {
+        g_printerr("Failed to get JSON root");
+        g_task_return_error(task, error);
+        goto cleanup;
+    }
+
+    results = match_json_path(response_root, "$['items'][0]['id']");
+    if(json_array_get_length(results) != 1) {
+        g_printerr("Unexpected channel ID results");
+        g_task_return_error(task, error);
+        goto cleanup;
+    }
+
+    const char* id = json_array_get_string_element(results, 0);
+    if(!id) {
+        g_printerr("Unexpected value for channel ID");
+        g_task_return_error(task, error);
+        goto cleanup;
+    }
+    char* id_str = g_strdup(id);
+    g_task_return_pointer(task, id_str, g_object_unref);
+cleanup:
+    json_array_unref(results);
+    json_node_unref(response_root);
+    g_bytes_unref(response);
+    g_object_unref(task);
+}
+
+// TODO: make cancellable if needed
+void youtube_chat_client_get_channel_id_async(YoutubeChatClient* client, const char* handle,
+                                              GCancellable* cancellable, GAsyncReadyCallback callback, gpointer data)
+{
+    Param params[] = {
+        {"part", "id"},
+        {"fields", "items/id"},
+        {"forHandle", handle}
+    };
+    SoupMessage* msg = create_youtube_message(client->api_key, "channels", params, cnt_of_array(params));
+    GTask* task = g_task_new(client, cancellable, callback, data);
+    soup_session_send_and_read_async(client->session, msg, G_PRIORITY_DEFAULT, cancellable, on_channel_id_response, task);
+}
+
+char* youtube_chat_client_get_channel_id_finish(YoutubeChatClient* client, GAsyncResult* result, GError** error)
+{
+    g_return_val_if_fail(g_task_is_valid(result, client), NULL);
+    return g_task_propagate_pointer(G_TASK(result), error);
+}
+
+static
+void on_live_streams_response(GObject* source_object, GAsyncResult* result, gpointer data)
+{
+    GError* error = NULL;
+    SoupSession* session = SOUP_SESSION(source_object);
+    GTask* task = (GTask*)data;
+    GBytes* response = soup_session_send_and_read_finish(session, result, &error);
+    JsonNode* response_root = NULL;
+    JsonArray* results = NULL;
+
+    if(error) {
+        g_printerr("Failed to fetch data");
+        g_task_return_error(task, error);
+        goto cleanup;
+    }
+
+    response_root = parse_json(response);
+    if(!response_root) {
+        g_printerr("Failed to get JSON root");
+        g_task_return_error(task, error);
+        goto cleanup;
+    }
+    results = match_json_path(response_root, "$['items'][*]['id']['videoId']");
+    GPtrArray* live_streams = g_ptr_array_new();
+    guint result_count = json_array_get_length(results);
+    for(guint i = 0; i < result_count; ++i) {
+        const char* video_id = json_array_get_string_element(results, i);
+        if(!video_id) {
+            g_warning("Unexpected format for video ID");
+            continue;
+        }
+        g_ptr_array_add(live_streams, g_strdup(video_id));
+    }
+    g_task_return_pointer(task, live_streams, g_object_unref);
+cleanup:
+    g_bytes_unref(response);
+    json_array_unref(results);
+    json_node_unref(response_root);
+    g_object_unref(task);
+}
+
+void youtube_chat_client_get_live_streams_async(YoutubeChatClient* client, char* channel_id,
+                                                GCancellable* cancellable, GAsyncReadyCallback callback, gpointer data)
+{
+    Param params[] = {
+        {"part", "snippet,id"},
+        {"fields","items/id/videoId"},
+        {"eventType", "live"},
+        {"channelId", channel_id},
+        {"order", "date"},
+        {"type", "video"}
+    };
+    SoupMessage* msg = create_youtube_message(client->api_key, "search", params, cnt_of_array(params));
+    GTask* task = g_task_new(client, cancellable, callback, data);
+    soup_session_send_and_read_async(client->session, msg, G_PRIORITY_DEFAULT, cancellable, on_live_streams_response, task);
+    g_free(channel_id);
+}
+
+GPtrArray* youtube_chat_client_get_live_streams_finish(YoutubeChatClient* client, GAsyncResult* result, GError** error)
+{
+    g_return_val_if_fail(g_task_is_valid(result, client), NULL);
+    return g_task_propagate_pointer(G_TASK(result), error);
+}
+
+static
+SoupMessage* create_youtube_message(const char* api_key, const char* resource, const Param* params, int param_count)
+{
+    GString* url = g_string_new(YOUTUBE_API_BASE_URL);
+    g_string_append(url, resource);
+    if(param_count > 0) {
+        g_string_append(url, "?");
+        for(int i = 0; i < param_count; ++i) {
+            g_string_append_uri_escaped(url, params[i].name, NULL, TRUE);
+            g_string_append(url, "=");
+            g_string_append_uri_escaped(url, params[i].value, NULL, TRUE);
+            if(i != param_count - 1) {
+                g_string_append(url, "&");
+            }
+        }
+    }
+    SoupMessage* request = soup_message_new(SOUP_METHOD_GET, url->str);
+    g_string_free(url, TRUE);
+    SoupMessageHeaders* headers = soup_message_get_request_headers(request);
+    soup_message_headers_append(headers, "x-goog-api-key", api_key);
+    return request;
+}
+
+static
+JsonNode* parse_json(GBytes* data)
+{
+    GError* err = NULL;
+    gsize data_size = 0;
+    gconstpointer data_ptr = g_bytes_get_data(data, &data_size);
+    JsonParser* parser = json_parser_new_immutable();
+    JsonNode* response_root = NULL;
+
+    json_parser_load_from_data(parser, data_ptr, data_size, &err);
+    if(err) {
+        g_printerr("Failed to parse JSON response");
+        goto cleanup;
+    }
+
+    response_root = json_parser_get_root(parser);
+    if(!response_root) {
+        g_printerr("Failed to get JSON root");
+        goto cleanup;
+    }
+cleanup:
+    json_node_ref(response_root);
+    g_object_unref(parser);
+    return response_root;
+}
+
+static
+JsonArray* match_json_path(JsonNode* root, const char* path)
+{
+    JsonPath* path_obj = json_path_new();
+    json_path_compile(path_obj, path, NULL);
+    JsonNode* results = json_path_match(path_obj, root);
+    JsonArray* results_array = json_node_get_array(results);
+    json_array_ref(results_array);
+    json_node_unref(results);
+    g_clear_object(&path_obj);
+    return results_array;
+}
