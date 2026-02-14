3ef7c86ae339adc92957d4fc72c2ea0c53773297
Cole Blakley <cblakley15@gmail.com>
2026-02-13 20:51:31 -0700

    Switch to use LibRest to reduce boilerplate
    LibRest handles constructing URLs and making HTTP requests for us.
    It also does some of the auth stuff (if we need OAuth in future)
    for us

--- youtube_chat_client.c
+++ youtube_chat_client.c
@@ -1,23 +1,16 @@
 #include "youtube_chat_client.h"
 #include <glib.h>
 #include <json-glib/json-glib.h>
-#include <libsoup/soup.h>
+#include <rest/rest.h>
 
 // TODO: https://developers.google.com/youtube/v3/live/docs/liveChatMessages
 //  - Seems like will have to poll using pollingIntervalMillis and nextPageToken
 
 #define YOUTUBE_API_BASE_URL "https://www.googleapis.com/youtube/v3/"
 
-#define cnt_of_array(arr) (sizeof(arr) / sizeof(arr[0]))
-
-typedef struct {
-    const char* name;
-    const char* value;
-} Param;
-
 struct _YoutubeChatClient {
     GObject parent_instance;
-    SoupSession* session;
+    RestProxy* proxy;
     char* api_key;
 };
 G_DEFINE_TYPE(YoutubeChatClient, youtube_chat_client, G_TYPE_OBJECT)
@@ -26,10 +19,7 @@ G_DEFINE_TYPE(YoutubeChatClient, youtube_chat_client, G_TYPE_OBJECT)
 G_DEFINE_QUARK(youtube-chat-error-quark, youtube_chat_error)
 
 static
-SoupMessage* create_youtube_message(const char* api_key, const char* resource, const Param* params, int param_count);
-
-static
-JsonNode* parse_json(GBytes* data, GError** error);
+JsonNode* parse_json(const char* data, gssize data_len, GError** error);
 
 static
 JsonArray* match_json_path(JsonNode* root, const char* path);
@@ -38,14 +28,14 @@ JsonArray* match_json_path(JsonNode* root, const char* path);
 static
 void youtube_chat_client_init(YoutubeChatClient* client)
 {
-    client->session = soup_session_new();
+    client->proxy = rest_proxy_new(YOUTUBE_API_BASE_URL, /*binding_required=*/FALSE);
 }
 
 static
 void youtube_chat_client_finalize(GObject* obj)
 {
     YoutubeChatClient* client = YOUTUBE_CHAT_CLIENT(obj);
-    g_clear_object(&client->session);
+    g_clear_object(&client->proxy);
     g_free(client->api_key);
     G_OBJECT_CLASS(youtube_chat_client_parent_class)->finalize(obj);
 }
@@ -75,18 +65,20 @@ static
 void on_channel_id_response(GObject* source_object, GAsyncResult* result, gpointer data)
 {
     GError* error = NULL;
-    SoupSession* session = SOUP_SESSION(source_object);
-    GTask* task = (GTask*)data;
-    GBytes* response = soup_session_send_and_read_finish(session, result, &error);
+    RestProxyCall* call = REST_PROXY_CALL(source_object);
+    GTask* task = data;
     JsonNode* response_root = NULL;
     JsonArray* results = NULL;
 
+    rest_proxy_call_invoke_finish(call, result, &error);
     if(error) {
         g_task_return_error(task, error);
         goto cleanup;
     }
 
-    response_root = parse_json(response, &error);
+    const char* response = rest_proxy_call_get_payload(call);
+    gssize response_len = rest_proxy_call_get_payload_length(call);
+    response_root = parse_json(response, response_len, &error);
     if(!response_root) {
         g_task_return_error(task, error);
         goto cleanup;
@@ -110,22 +102,25 @@ void on_channel_id_response(GObject* source_object, GAsyncResult* result, gpoint
 cleanup:
     json_array_unref(results);
     json_node_unref(response_root);
-    g_bytes_unref(response);
     g_object_unref(task);
+    g_object_unref(call);
 }
 
 // TODO: make cancellable if needed
 void youtube_chat_client_get_channel_id_async(YoutubeChatClient* client, const char* handle,
                                               GCancellable* cancellable, GAsyncReadyCallback callback, gpointer data)
 {
-    Param params[] = {
-        {"part", "id"},
-        {"fields", "items/id"},
-        {"forHandle", handle}
-    };
-    SoupMessage* msg = create_youtube_message(client->api_key, "channels", params, cnt_of_array(params));
+    RestProxyCall* call = rest_proxy_new_call(client->proxy);
+    rest_proxy_call_add_header(call, "x-goog-api-key", client->api_key);
+    rest_proxy_call_add_params(call,
+        "part", "id",
+        "fields", "items/id",
+        "forHandle", handle,
+        NULL);
+    rest_proxy_call_set_function(call, "channels");
+
     GTask* task = g_task_new(client, cancellable, callback, data);
-    soup_session_send_and_read_async(client->session, msg, G_PRIORITY_DEFAULT, cancellable, on_channel_id_response, task);
+    rest_proxy_call_invoke_async(call, cancellable, on_channel_id_response, task);
 }
 
 char* youtube_chat_client_get_channel_id_finish(YoutubeChatClient* client, GAsyncResult* result, GError** error)
@@ -138,18 +133,20 @@ static
 void on_live_streams_response(GObject* source_object, GAsyncResult* result, gpointer data)
 {
     GError* error = NULL;
-    SoupSession* session = SOUP_SESSION(source_object);
-    GTask* task = (GTask*)data;
-    GBytes* response = soup_session_send_and_read_finish(session, result, &error);
+    RestProxyCall* call = REST_PROXY_CALL(source_object);
+    GTask* task = data;
     JsonNode* response_root = NULL;
     JsonArray* results = NULL;
 
+    rest_proxy_call_invoke_finish(call, result, &error);
     if(error) {
         g_task_return_error(task, error);
         goto cleanup;
     }
 
-    response_root = parse_json(response, &error);
+    const char* response = rest_proxy_call_get_payload(call);
+    gssize response_len = rest_proxy_call_get_payload_length(call);
+    response_root = parse_json(response, response_len, &error);
     if(error) {
         g_task_return_error(task, error);
         goto cleanup;
@@ -167,26 +164,29 @@ void on_live_streams_response(GObject* source_object, GAsyncResult* result, gpoi
     }
     g_task_return_pointer(task, live_streams, g_object_unref);
 cleanup:
-    g_bytes_unref(response);
     json_array_unref(results);
     json_node_unref(response_root);
     g_object_unref(task);
+    g_object_unref(call);
 }
 
 void youtube_chat_client_get_live_streams_async(YoutubeChatClient* client, char* channel_id,
                                                 GCancellable* cancellable, GAsyncReadyCallback callback, gpointer data)
 {
-    Param params[] = {
-        {"part", "snippet,id"},
-        {"fields","items/id/videoId"},
-        {"eventType", "live"},
-        {"channelId", channel_id},
-        {"order", "date"},
-        {"type", "video"}
-    };
-    SoupMessage* msg = create_youtube_message(client->api_key, "search", params, cnt_of_array(params));
+    RestProxyCall* call = rest_proxy_new_call(client->proxy);
+    rest_proxy_call_add_header(call, "x-goog-api-key", client->api_key);
+    rest_proxy_call_add_params(call,
+        "part", "snippet,id",
+        "fields","items/id/videoId",
+        "eventType", "live",
+        "channelId", channel_id,
+        "order", "date",
+        "type", "video",
+        NULL);
+    rest_proxy_call_set_function(call, "search");
+
     GTask* task = g_task_new(client, cancellable, callback, data);
-    soup_session_send_and_read_async(client->session, msg, G_PRIORITY_DEFAULT, cancellable, on_live_streams_response, task);
+    rest_proxy_call_invoke_async(call, cancellable, on_live_streams_response, task);
     g_free(channel_id);
 }
 
@@ -197,37 +197,12 @@ GPtrArray* youtube_chat_client_get_live_streams_finish(YoutubeChatClient* client
 }
 
 static
-SoupMessage* create_youtube_message(const char* api_key, const char* resource, const Param* params, int param_count)
+JsonNode* parse_json(const char* data, gssize data_len, GError** error)
 {
-    GString* url = g_string_new(YOUTUBE_API_BASE_URL);
-    g_string_append(url, resource);
-    if(param_count > 0) {
-        g_string_append(url, "?");
-        for(int i = 0; i < param_count; ++i) {
-            g_string_append_uri_escaped(url, params[i].name, NULL, TRUE);
-            g_string_append(url, "=");
-            g_string_append_uri_escaped(url, params[i].value, NULL, TRUE);
-            if(i != param_count - 1) {
-                g_string_append(url, "&");
-            }
-        }
-    }
-    SoupMessage* request = soup_message_new(SOUP_METHOD_GET, url->str);
-    g_string_free(url, TRUE);
-    SoupMessageHeaders* headers = soup_message_get_request_headers(request);
-    soup_message_headers_append(headers, "x-goog-api-key", api_key);
-    return request;
-}
-
-static
-JsonNode* parse_json(GBytes* data, GError** error)
-{
-    gsize data_size = 0;
-    gconstpointer data_ptr = g_bytes_get_data(data, &data_size);
     JsonParser* parser = json_parser_new_immutable();
     JsonNode* response_root = NULL;
 
-    json_parser_load_from_data(parser, data_ptr, data_size, error);
+    json_parser_load_from_data(parser, data, data_len, error);
     if(*error) {
         goto cleanup;
     }
